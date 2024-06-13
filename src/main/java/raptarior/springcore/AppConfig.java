f2af2ef9219de9b341707b2e6fe678efcae9a6e6
raptarior <qazqww@naver.com>
2024-06-13 17:32:23 +0900

    Add: Spring Container

--- src/main/java/raptarior/springcore/AppConfig.java
+++ src/main/java/raptarior/springcore/AppConfig.java
@@ -1,5 +1,7 @@
 package raptarior.springcore;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import raptarior.springcore.discount.DiscountPolicy;
 import raptarior.springcore.discount.FixDiscountPolicy;
 import raptarior.springcore.discount.RateDiscountPolicy;
@@ -10,21 +12,27 @@ import raptarior.springcore.member.MemoryMemberRepository;
 import raptarior.springcore.order.OrderService;
 import raptarior.springcore.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/raptarior/springcore/OrderApp.java
+++ src/main/java/raptarior/springcore/OrderApp.java
@@ -1,5 +1,7 @@
 package raptarior.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import raptarior.springcore.member.Grade;
 import raptarior.springcore.member.Member;
 import raptarior.springcore.member.MemberService;
@@ -10,9 +12,13 @@ import raptarior.springcore.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
